829113c618c7154e8298d2aaefe3ce7a65e22021
Chris Kalfas <christopherkalfas22@gmail.com>
2020-08-03 12:32:17 -0400

    Build out signup/login forms and imported then app component. Also wrote out post request for user profiles

---
+++ mystagram2point0-client/src/App.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import axios from 'axios'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import Signup from "./components/registrations/Signup"
+import Login from "./components/registrations/Login"
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  }
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+ handleLogin = (data) => {
+  this.setState({
+    isLoggedIn: true,
+    user: data.user
+  })
+  }
+ handleLogout = ()=> {
+  this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
+
+loginStatus() {
+    axios.get('http://localhost:3001/logged_in',
+    {withCredentials: true})
+
+    .then(resp => {
+      if (resp.data.logged_in){
+        this.handleLogin(resp)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
+
+
+  render(){
+
+    return (
+      <div>
+        <h1>TESTING</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={props => (
+              <Home {...props} loggedInStatus={this.state.isLoggedIn} />
+            )} />
+            <Route exact path='/login' render={props =>(
+              <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+            )}/>
+            <Route exact path='/signup' render={props =>(
+              <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+            )}/>
+          </Switch>
+      </BrowserRouter>
+    </div>
+  );
+  }  
+}
+export default App;
